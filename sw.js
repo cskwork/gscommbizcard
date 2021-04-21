dd709eb6df9e33b5c56f163b96582cc022c2f277
cskwork <csk917work@gmail.com>
2021-04-21 16:03:45 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -24,3 +24,19 @@ self.addEventListener('fetch', function(e) {
     })
   );
 });
+
+// MAKE FASTER
+self.addEventListener('activate', (event) => {
+  console.log('[ServiceWorker] Activate');
+  event.waitUntil((async () => {
+    // Enable navigation preload if it's supported.
+    // See https://developers.google.com/web/updates/2017/02/navigation-preload
+    if ('navigationPreload' in self.registration) {
+      await self.registration.navigationPreload.enable();
+    }
+  })());
+
+  // Tell the active service worker to take control of the page immediately.
+  self.clients.claim();
+});
+
