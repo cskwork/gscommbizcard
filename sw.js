6791df57185e82e600ce65c930873b6098f0aad2
cskwork <csk917work@gmail.com>
2021-04-21 14:03:05 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -9,7 +9,7 @@ var filesToCache = [
 /* Start the service worker and cache all of the app's content */
 self.addEventListener('install', function(e) {
   e.waitUntil(
-    caches.open(cacheName).then(function(cache) {
+    cache.open(cacheName).then(function(cache) {
       return cache.addAll(filesToCache);
     })
   );
@@ -19,7 +19,7 @@ self.addEventListener('install', function(e) {
 /* Serve cached content when offline */
 self.addEventListener('fetch', function(e) {
   e.respondWith(
-    caches.match(e.request).then(function(response) {
+    cache.match(e.request).then(function(response) {
       return response || fetch(e.request);
     })
   );
