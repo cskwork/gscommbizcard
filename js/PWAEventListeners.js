d890a681190b90e8f10cf4b3a8effb806bc1fe8d
cskwork <csk917work@gmail.com>
2021-04-21 14:53:34 +0900

    Update PWAEventListeners.js

--- js/PWAEventListeners.js
+++ js/PWAEventListeners.js
@@ -1,6 +1,6 @@
 let deferredPrompt;
 
-window.addEventListener('beforeinstallprompt', (e) => {
+self.addEventListener('beforeinstallprompt', (e) => {
   // Prevent Chrome 67 and earlier from automatically showing the prompt
   e.preventDefault();
   // Stash the event so it can be triggered later.
